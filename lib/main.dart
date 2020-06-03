8d5e21d67563f92c9ad314c8a6fd854a279dea68
Andi Irham <Andi.irhamm@gmail.com>
2020-06-04 00:29:42 +0700

    [Andi-IM] updating purpose

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello Flutter!'),
+        appBar: new AppBar(
+            backgroundColor: Colors.red,
+            leading: new Icon(Icons.home),
+            title: new Text("Andi Irham"),
+            actions: <Widget>[
+              new Center(
+                  child: new Text(
+                "Home",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                ),
+              )),
+              new Center(
+                  child: new Text(
+                "About",
+                style: new TextStyle(fontSize: 20.0),
+              )),
+              new Center(
+                  child: new Text(
+                "Contact",
+                style: new TextStyle(fontSize: 20.0),
+              )),
+            ]),
+        body: new Column(
+          children: [
+            Container(
+              height: 90.0,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 500.0,
+              color: Colors.amber,
+              child: new Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 200.0,
+                    color: Colors.blue[400],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
